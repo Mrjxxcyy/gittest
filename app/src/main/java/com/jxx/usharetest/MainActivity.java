616e732bee6cd740b4ae8f9d6d198754d4a3d980
jxx <jixiaoxu@synjones.net>
2017-03-27 20:06:34 +0800

    第一次提交,gittest

---
+++ app/src/main/java/com/jxx/usharetest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jxx.usharetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        key:58cb80e27f2c74521100171e
+    }
+}
